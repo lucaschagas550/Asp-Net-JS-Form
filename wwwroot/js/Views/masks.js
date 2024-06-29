bc1a89afebc3507830b4a2d2bc213c45d02ed6df
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-06-28 22:16:50 -0300

    feat: mascara para valores negativos

--- wwwroot/js/Views/masks.js
+++ wwwroot/js/Views/masks.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
     $('#number').mask('000')
     $('.money').mask('000.000.000.000.000,00', { reverse: true });
     $('#time').mask('00:00');
+
+
+    // Adicionando máscara para dinheiro com sinal negativo opcional e limite de 18,2 decimais
+
+    $('.money-negative').maskMoney({
+        prefix: '',
+        allowNegative: true,
+        thousands: '.',
+        decimal: ',',
+        affixesStay: false,
+        precision: 2 // Define a precisão para 2 casas decimais
+    });
+
+    // Limita o campo de entrada a 26 caracteres: 18 dígitos, 2 casas decimais e 1 sinal negativo
+    $('.money-negative').attr('maxlength', 26);
 });
